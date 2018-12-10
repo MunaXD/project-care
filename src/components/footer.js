2153e58b99075982d9585dfd746aa5b2585f3d60
Amin Shah Gilani <amin@gilani.me>
2018-12-11 00:38:44 +0500

    Update footer with links to license, submission and code

--- src/components/footer.js
+++ src/components/footer.js
@@ -9,11 +9,14 @@ const Footer = ({siteTitle}) => (
             hours for the <a href="https://2018isbhacks.devpost.com">Jazz SDG Hackathon 2018</a>.
           </p>
           <p>
-
-          © All content is copyright <a href="https://amin.gilani.me">Amin Gilani</a>, <a href="https://www.linkedin.com/in/mohsulaimankhan/">Sulaiman Khan</a>, and <a href="https://www.linkedin.com/in/izzashahid-gilani/">Izza Gilani</a>, 2018. <br />
-
-          All original content imagery is released under a Creative Commons Attribution-NonCommercial 4.0 International License. <br />
-          All code is released under the AGPL v3.
+            <a href="https://github.com/amingilani/project-care"><i className="github icon"></i> Code on Github</a> | <a href="https://devpost.com/software/project-care"><i className="code icon"></i> DevPost Submission</a>
+          </p>
+          <p>
+          © All content is copyright <a href="https://amin.gilani.me">Amin Gilani</a>, <a href="https://www.linkedin.com/in/mohsulaimankhan/">Sulaiman Khan</a>, and <a href="https://www.linkedin.com/in/izzashahid-gilani/">Izza Gilani</a>, 2018.
+          </p>
+          <p>
+          All original content imagery is released under <a href="https://creativecommons.org/licenses/by-nc/4.0/">CC BY-NC 4.0.</a> <br />
+          All code is released under the <a href="https://www.gnu.org/licenses/agpl.txt">AGPL v3</a>.
           </p>
       </div>
     </div>
